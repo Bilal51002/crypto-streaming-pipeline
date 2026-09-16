4a90963f0a430eb23e4562e1600971d0639516d8
Bilal KHALLABI <bilalelkhalabi93@gmail.com>
2026-09-16 15:30:41 +0100

    Delay Spark job start until Cassandra keyspace and tables exist

--- spark/spark_stream.py
+++ spark/spark_stream.py
@@ -28,7 +28,7 @@ def main():
         .format("kafka")
         .option("kafka.bootstrap.servers", KAFKA_BROKER)
         .option("subscribe", KAFKA_TOPIC)
-        .option("startingOffsets", "latest")
+        .option("startingOffsets", "earliest")
         .load()
     )
 
